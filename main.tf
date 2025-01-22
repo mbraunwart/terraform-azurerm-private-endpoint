bef36333e9fb5120ffce9deb7752d2845be0491b
mbraunwart <matt.braunwart@gmail.com>
2025-01-22 19:39:31 +0000

    add dns zone

--- main.tf
+++ main.tf
@@ -26,6 +26,8 @@ data "azurerm_virtual_network" "vnet" {
 resource "azurerm_private_dns_zone" "zone" {
   name                = var.dns_zone_name
   resource_group_name = data.azurerm_resource_group.vnet_rg.name
+
+  tags = merge(var.tags, { "service" = "private-dns-zone" })
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "link" {
